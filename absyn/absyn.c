b2de12d88aaca217f5edba667da4e730f00cdf54
Maryeon <739759682@qq.com>
2018-06-03 16:40:19 +0800

    add implementation of A_var

--- absyn/absyn.c
+++ absyn/absyn.c
@@ -1,1 +1,21 @@
 #include "absyn.h"
+#include <stdlib.h>
+
+A_var A_SimpleVar(A_pos pos, S_symbol sym)
+{
+	A_var p = (A_var)checked_malloc(sizeof(*p));
+	p->kind = A_simpleVar;
+	p->pos = pos;
+	p->u.simple = sym;
+	return p;
+}
+
+A_var A_SubscriptVar(A_pos pos, A_var var, A_exp exp)
+{
+	A_var p = (A_var)checked_malloc(sizeof(*p));
+	p->kind = A_subscriptVar;
+	p->pos = pos;
+	p->u.subscript.var = var;
+	p->u.subscript.exp = exp;
+	return p;
+}
