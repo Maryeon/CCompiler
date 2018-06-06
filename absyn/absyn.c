7b9f5b22ca63e444370a3b293f3afd1fc4954d64
Maryeon <739759682@qq.com>
2018-06-06 11:14:18 +0800

    add a construct example

--- absyn/absyn.c
+++ absyn/absyn.c
@@ -17,7 +17,25 @@ A_translation_unit A_Translation_Unit(A_pos pos, int grammer, void *argv[])
 				p->u.u2.u2 = (A_external_declaration)argv[1];
 				break;
 		default:
+				return NULL;
+	}
+	
+	return p;
+}
+
+A_external_declaration A_External_Declaration(A_pos, int grammer, void *argv[])
+{
+	A_external_declaration p = checked_malloc(sizeof(*p));
+	p->grammer = grammer;
+	p->pos = pos;
+	switch(grammer)
+	{
+		case 1:	p->u.u1 = argv[0];
 				break;
+		case 2: p->u.u2 = argv[1];
+				break;
+		default:
+				return NULL;
 	}
 	
 	return p;
