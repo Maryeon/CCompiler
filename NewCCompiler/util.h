982bacce4c0bc38e8ba99db1815d4d32df461b84
Shuang Guo <454245092@qq.com>
2018-06-10 15:13:06 +0800

    add NewCCompiler

---
+++ NewCCompiler/util.h
@@ -0,0 +1,13 @@
+#ifndef UTIL_H
+#define	UTIL_H
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char *string;
+typedef const char* conststring;
+
+void *checked_malloc(int);
+
+#endif
